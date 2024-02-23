82c44ecf8e059edc392f5be446a3f2784ae0ac71
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2024-02-23 16:52:01 -0500

    caso Malala

--- proyecto/fase03/proyectoWebJava/CalculadoraApp/src/main/java/serviparamo/calculadoraapp/CalculadoraApp.java
+++ proyecto/fase03/proyectoWebJava/CalculadoraApp/src/main/java/serviparamo/calculadoraapp/CalculadoraApp.java
@@ -1,9 +1,13 @@
-
+       
 package serviparamo.calculadoraapp;
+import java.util.Scanner;
 
 public class CalculadoraApp {
 
     public static void main(String[] args) {
+        Scanner teclado= new Scanner(System.in);
+        int opcion;
+        
         System.out.println("------Calculadora App --------");
         System.out.println("             MENU             ");
     
@@ -14,5 +18,24 @@ public class CalculadoraApp {
                            4. Dividir
                            5. Salir
                            """);
+        System.out.print("Digige opcion del 1 al 5 : ");
+        opcion=teclado.nextInt();
+        switch (opcion) {
+            case 1:
+                System.out.println("Opcion 1 Suma");
+                break;
+            case 2:
+                System.out.println("opcion 2 Resta");
+                break;
+            case 3:
+                System.out.println("opcion 3 multiplicacion");
+                break;
+            case 4:
+                System.out.println("opcion 4 Division");
+                break;
+            default: System.out.println("opcion no valida");
+               // throw new AssertionError();
+        }
     }
+    
 }
